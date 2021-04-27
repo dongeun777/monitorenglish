677f18bba52e308b258e35bda1f19db76dd80cdc
Yong0917 <ssyong917@naver.com>
2021-04-27 17:40:13 +0900

    Yong test

---
+++ monitor/src/main/java/igloosec/monitor/controller/ResourceController.java
@@ -0,0 +1,35 @@
+package igloosec.monitor.controller;
+
+import igloosec.monitor.service.ResourceService;
+import igloosec.monitor.vo.ResourceVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class ResourceController {
+
+    private final ResourceService resourceService;
+    @Autowired
+    public ResourceController(ResourceService resourceService) {
+        this.resourceService = resourceService;
+    }
+
+
+    @ResponseBody
+    @RequestMapping(value = "/getResourceList.do")
+    public List<ResourceVo> getResourceList(Model model) {
+
+
+        List<ResourceVo>  list = resourceService.selectResourceList();
+
+        model.addAttribute("list",list);
+
+        return list;
+    }
+}
